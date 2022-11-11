b2ffd52fc69add979143655bf59ac766542ec97b
PhamThanhDuongTH20C <113371301+PhamThanhDuongTH20C@users.noreply.github.com>
2022-11-11 23:16:19 +0700

    Update_code_thong_tin_user

--- lib/Model_Thong_tin_user/Thong_Tin_user.dart
+++ lib/Model_Thong_tin_user/Thong_Tin_user.dart
@@ -10,3 +10,45 @@ class Thong_Tin_User extends StatefulWidget {
   @override
   State<Thong_Tin_User> createState() => _Thong_Tin_User();
 }
+
+class _Thong_Tin_User extends State<Thong_Tin_User> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height,
+              decoration: const BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage('Images/h2.jpg'), fit: BoxFit.fill)),
+            ),
+            SingleChildScrollView(
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Column(
+                        children: [
+                          Icon(
+                            Icons.home,
+                            size: 30,
+                          )
+                        ],
+                      ),
+                      Expanded(
+                          child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Image(
+                            image: AssetImage('Images/h2.png'),
+                            width: MediaQuery.of(context).size.width / 2.5,
+                          )
+                        ],
+                      ))
+                    ],
+                  ),
+            
