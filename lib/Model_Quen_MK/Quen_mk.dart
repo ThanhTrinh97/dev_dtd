fe44d9fbfde935aed55fe7a7aac8480db5f269ac
PhamThanhDuongTH20C <113371301+PhamThanhDuongTH20C@users.noreply.github.com>
2022-11-11 23:10:15 +0700

    Khai_Bao_Quen_MK

---
+++ lib/Model_Quen_MK/Quen_mk.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+bool light = true;
+bool valuesecond = false;
+bool isChecked = false;
+
+class Quen_MK extends StatefulWidget {
+  const Quen_MK({super.key});
+  @override
+  State<Quen_MK> createState() => _Quen_MK();
+}
