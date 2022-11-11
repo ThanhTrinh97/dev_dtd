58850fa2228e273c685ec80b6f09ea0c77b1acba
PhamThanhDuongTH20C <113371301+PhamThanhDuongTH20C@users.noreply.github.com>
2022-11-11 23:16:57 +0700

    Update_code_thong_tin_user

--- lib/Model_Thong_tin_user/Thong_Tin_user.dart
+++ lib/Model_Thong_tin_user/Thong_Tin_user.dart
@@ -51,4 +51,159 @@ class _Thong_Tin_User extends State<Thong_Tin_User> {
                       ))
                     ],
                   ),
-            
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'AVATAR',
+                        style: TextStyle(
+                            color: Color.fromARGB(255, 232, 2, 2),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 27),
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: TextField(
+                      style: TextStyle(color: Colors.blue),
+                      decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(width: 1, color: Colors.black),
+                          ),
+                          border: OutlineInputBorder(),
+                          labelText: 'HỌ VÀ TÊN',
+                          prefixIcon: Icon(
+                            Icons.add_outlined,
+                            color: Colors.white,
+                          ),
+                          labelStyle: TextStyle(
+                              color: Colors.yellow,
+                              fontWeight: FontWeight.w500)),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: TextField(
+                      style: TextStyle(color: Colors.blue),
+                      decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(width: 1, color: Colors.black),
+                          ),
+                          border: OutlineInputBorder(),
+                          labelText: 'LEVEL',
+                          prefixIcon: Icon(
+                            Icons.keyboard_arrow_right,
+                            color: Colors.white,
+                          ),
+                          labelStyle: TextStyle(
+                              color: Colors.yellow,
+                              fontWeight: FontWeight.w500)),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: TextField(
+                      style: TextStyle(color: Colors.blue),
+                      decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(width: 1, color: Colors.black),
+                          ),
+                          border: OutlineInputBorder(),
+                          prefixIcon: Icon(
+                            Icons.phone,
+                            color: Colors.white,
+                          ),
+                          labelText: 'SỐ ĐIỆN THOẠI',
+                          labelStyle: TextStyle(
+                              color: Colors.yellow,
+                              fontWeight: FontWeight.w500)),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: TextField(
+                      style: TextStyle(color: Colors.blue),
+                      decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(width: 1, color: Colors.black),
+                          ),
+                          border: OutlineInputBorder(),
+                          prefixIcon: Icon(
+                            Icons.date_range,
+                            color: Colors.white,
+                          ),
+                          labelText: 'NĂM SINH',
+                          labelStyle: TextStyle(
+                              color: Colors.yellow,
+                              fontWeight: FontWeight.w500)),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(5.0),
+                    child: TextField(
+                      style: TextStyle(color: Colors.blue),
+                      decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderSide:
+                                BorderSide(width: 1, color: Colors.black),
+                          ),
+                          border: OutlineInputBorder(),
+                          prefixIcon: Icon(
+                            Icons.lock,
+                            color: Colors.white,
+                          ),
+                          labelText: 'MẬT KHẨU CẤP 2',
+                          suffixIcon: Icon(Icons.remove_red_eye),
+                          labelStyle: TextStyle(
+                              color: Colors.yellow,
+                              fontWeight: FontWeight.w500)),
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Expanded(
+                          child: Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Column(
+                          children: [
+                            ElevatedButton(
+                              style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStatePropertyAll<Color>(
+                                        Colors.red.withOpacity(0.8)),
+                                shape: MaterialStateProperty.all(
+                                  RoundedRectangleBorder(
+                                      borderRadius:
+                                          BorderRadius.circular(30.0)),
+                                ),
+                              ),
+                              onPressed: () {},
+                              child: const Padding(
+                                padding: EdgeInsets.fromLTRB(50, 15, 50, 15),
+                                child: Text(
+                                  'CẬP NHẬT THÔNG TIN',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ))
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
