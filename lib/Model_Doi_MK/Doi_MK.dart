8f4ff30f4848759dda5a25e8aaad84697848c48b
PhamThanhDuongTH20C <113371301+PhamThanhDuongTH20C@users.noreply.github.com>
2022-11-11 23:12:56 +0700

    Update_code_Doi_MK

--- lib/Model_Doi_MK/Doi_MK.dart
+++ lib/Model_Doi_MK/Doi_MK.dart
@@ -10,3 +10,166 @@ class Doi_MK extends StatefulWidget {
   @override
   State<Doi_MK> createState() => _Doi_MK();
 }
+
+class _Doi_MK extends State<Doi_MK> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: AssetImage('Images/h2.jpg'), fit: BoxFit.fill),
+              ),
+            ),
+            SingleChildScrollView(
+              child: Column(
+                children: [
+                  Column(
+                    children: [
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Column(
+                            children: [
+                              Icon(
+                                Icons.home,
+                                size: 30,
+                              )
+                            ],
+                          ),
+                          Expanded(
+                              child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Image(
+                                image: AssetImage('Images/h2.png'),
+                                width: MediaQuery.of(context).size.width / 2.5,
+                              )
+                            ],
+                          ))
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Text(
+                          'THAY ĐỔI MẬT KHẨU',
+                          style: TextStyle(
+                              color: Color.fromARGB(255, 232, 2, 2),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 25),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: TextField(
+                          style: TextStyle(color: Colors.blue),
+                          decoration: InputDecoration(
+                              enabledBorder: OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(width: 1, color: Colors.black),
+                              ),
+                              border: OutlineInputBorder(),
+                              labelText: 'MẬT KHẨU HIỆN TẠI',
+                              prefixIcon: Icon(
+                                Icons.lock,
+                                color: Colors.white,
+                              ),
+                              suffixIcon: Icon(Icons.remove_red_eye),
+                              labelStyle: TextStyle(
+                                  color: Colors.yellow,
+                                  fontWeight: FontWeight.bold)),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: TextField(
+                          style: TextStyle(color: Colors.blue),
+                          decoration: InputDecoration(
+                              enabledBorder: OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(width: 1, color: Colors.black),
+                              ),
+                              border: OutlineInputBorder(),
+                              labelText: 'MẬT KHẨU MỚI',
+                              prefixIcon: Icon(
+                                Icons.lock,
+                                color: Colors.white,
+                              ),
+                              suffixIcon: Icon(Icons.remove_red_eye),
+                              labelStyle: TextStyle(
+                                  color: Colors.yellow,
+                                  fontWeight: FontWeight.bold)),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: TextField(
+                          style: TextStyle(color: Colors.blue),
+                          decoration: InputDecoration(
+                              enabledBorder: OutlineInputBorder(
+                                borderSide:
+                                    BorderSide(width: 1, color: Colors.black),
+                              ),
+                              border: OutlineInputBorder(),
+                              labelText: 'NHẬP LẠI MẬT KHẨU MỚI',
+                              prefixIcon: Icon(
+                                Icons.lock,
+                                color: Colors.white,
+                              ),
+                              suffixIcon: Icon(Icons.remove_red_eye),
+                              labelStyle: TextStyle(
+                                  color: Colors.yellow,
+                                  fontWeight: FontWeight.bold)),
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Expanded(
+                              child: Padding(
+                            padding: const EdgeInsets.all(10),
+                            child: Column(
+                              children: [
+                                ElevatedButton(
+                                  style: ButtonStyle(
+                                    backgroundColor:
+                                        MaterialStatePropertyAll<Color>(
+                                            Colors.red.withOpacity(0.8)),
+                                    shape: MaterialStateProperty.all(
+                                      RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(30.0)),
+                                    ),
+                                  ),
+                                  onPressed: () {},
+                                  child: const Padding(
+                                    padding:
+                                        EdgeInsets.fromLTRB(50, 15, 50, 15),
+                                    child: Text(
+                                      'ĐỔI MẬT KHẨU',
+                                      style: TextStyle(
+                                          color: Colors.white,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ))
+                        ],
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
