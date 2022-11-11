14df0b10c07a4b35a7caae489855a64cdda70a3f
PhamThanhDuongTH20C <113371301+PhamThanhDuongTH20C@users.noreply.github.com>
2022-11-11 23:15:29 +0700

    Khai_Báo_Thong_tin_user

---
+++ lib/Model_Thong_tin_user/Thong_Tin_user.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+bool light = true;
+bool valuesecond = false;
+bool isChecked = false;
+
+class Thong_Tin_User extends StatefulWidget {
+  const Thong_Tin_User({super.key});
+  @override
+  State<Thong_Tin_User> createState() => _Thong_Tin_User();
+}
