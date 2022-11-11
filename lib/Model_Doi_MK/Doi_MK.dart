b95d595424792e9cbee9e7be0af634073940f5b6
PhamThanhDuongTH20C <113371301+PhamThanhDuongTH20C@users.noreply.github.com>
2022-11-11 23:12:26 +0700

    Khai_Bao_Doi_Mk

---
+++ lib/Model_Doi_MK/Doi_MK.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+bool light = true;
+bool valuesecond = false;
+bool isChecked = false;
+
+class Doi_MK extends StatefulWidget {
+  const Doi_MK({super.key});
+  @override
+  State<Doi_MK> createState() => _Doi_MK();
+}
