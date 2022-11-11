3a08cd7b3bc2729ba45574f4d38bf0ef8305a47e
ThanhTrinh97 <113372023+ThanhTrinh97@users.noreply.github.com>
2022-11-11 23:37:13 +0700

    add history.dart

---
+++ components/history.dart
@@ -0,0 +1,271 @@
+import 'package:flutter/material.dart';
+import 'menu.dart';
+
+class History extends StatefulWidget {
+  const History({super.key});
+
+  @override
+  State<History> createState() => _History();
+}
+
+class _History extends State<History> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('images/back.jpg'),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+            SingleChildScrollView(
+              child: Column(
+                children: [
+                  Container(
+                    padding: EdgeInsets.fromLTRB(5, 10, 5, 10),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Text(
+                            'Lịch sử trận đấu',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.white,
+                            ),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                        IconButton(
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const Menu()));
+                          },
+                          icon: Icon(
+                            Icons.clear,
+                            color: Colors.white,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  Column(
+                    children: [
+                      Container(
+                        margin: EdgeInsets.fromLTRB(10, 20, 10, 20),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                              width: 1.0,
+                              color: Colors.grey,
+                              style: BorderStyle.solid,
+                            ),
+                            borderRadius: BorderRadius.circular(5)),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              primary: Colors.white,
+                              onPrimary: Colors.black,
+                              minimumSize: Size(100, 50)),
+                          onPressed: () {},
+                          child: Row(
+                            children: [
+                              Expanded(
+                                  child: Row(
+                                children: [
+                                  Container(
+                                    margin: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                                    decoration: BoxDecoration(
+                                        border: Border.all(
+                                          width: 1.0,
+                                          style: BorderStyle.solid,
+                                        ),
+                                        borderRadius:
+                                            BorderRadius.circular(100)),
+                                    child: Icon(Icons.question_mark_outlined),
+                                  ),
+                                  Text('Số câu hỏi đã được trả lời'),
+                                ],
+                              )),
+                              Text('0'),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.fromLTRB(10, 20, 10, 20),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                              width: 1.0,
+                              color: Colors.grey,
+                              style: BorderStyle.solid,
+                            ),
+                            borderRadius: BorderRadius.circular(5)),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              primary: Colors.white,
+                              onPrimary: Colors.black,
+                              minimumSize: Size(100, 50)),
+                          onPressed: () {},
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Row(
+                                  children: [
+                                    Container(
+                                      margin: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                                      decoration: BoxDecoration(
+                                          border: Border.all(
+                                            width: 1.0,
+                                            style: BorderStyle.solid,
+                                          ),
+                                          borderRadius:
+                                              BorderRadius.circular(100)),
+                                      child: Icon(Icons.done_all),
+                                    ),
+                                    Text('Số câu hỏi trả lời đúng'),
+                                  ],
+                                ),
+                              ),
+                              Text('0'),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.fromLTRB(10, 20, 10, 20),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                              width: 1.0,
+                              color: Colors.grey,
+                              style: BorderStyle.solid,
+                            ),
+                            borderRadius: BorderRadius.circular(5)),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              primary: Colors.white,
+                              onPrimary: Colors.black,
+                              minimumSize: Size(100, 50)),
+                          onPressed: () {},
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Row(
+                                  children: [
+                                    Container(
+                                      margin: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                                      decoration: BoxDecoration(
+                                          border: Border.all(
+                                            width: 1.0,
+                                            style: BorderStyle.solid,
+                                          ),
+                                          borderRadius:
+                                              BorderRadius.circular(100)),
+                                      child: Icon(Icons.clear),
+                                    ),
+                                    Text('Số câu hỏi trả lời sai')
+                                  ],
+                                ),
+                              ),
+                              Text('0'),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.fromLTRB(10, 20, 10, 20),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                              width: 1.0,
+                              color: Colors.grey,
+                              style: BorderStyle.solid,
+                            ),
+                            borderRadius: BorderRadius.circular(5)),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              primary: Colors.white,
+                              onPrimary: Colors.black,
+                              minimumSize: Size(100, 50)),
+                          onPressed: () {},
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Row(
+                                  children: [
+                                    Container(
+                                      margin: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                                      decoration: BoxDecoration(
+                                          border: Border.all(
+                                            width: 1.0,
+                                            style: BorderStyle.solid,
+                                          ),
+                                          borderRadius:
+                                              BorderRadius.circular(100)),
+                                      child: Icon(Icons.person),
+                                    ),
+                                    Text('Trò chơi đã chơi')
+                                  ],
+                                ),
+                              ),
+                              Text('0'),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.fromLTRB(10, 20, 10, 20),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                              width: 1.0,
+                              color: Colors.grey,
+                              style: BorderStyle.solid,
+                            ),
+                            borderRadius: BorderRadius.circular(5)),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                              primary: Colors.white,
+                              onPrimary: Colors.black,
+                              minimumSize: Size(100, 50)),
+                          onPressed: () {},
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Row(
+                                  children: [
+                                    Container(
+                                      margin: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                                      decoration: BoxDecoration(
+                                          border: Border.all(
+                                            width: 1.0,
+                                            style: BorderStyle.solid,
+                                          ),
+                                          borderRadius:
+                                              BorderRadius.circular(100)),
+                                      child:
+                                          Icon(Icons.workspace_premium_sharp),
+                                    ),
+                                    Text('Chiến thắng trò chơi'),
+                                  ],
+                                ),
+                              ),
+                              Text('0')
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
