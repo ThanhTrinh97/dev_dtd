76d75c7e0ac196d3f8d9ee2002ac7b010161e888
ThanhTrinh97 <113372023+ThanhTrinh97@users.noreply.github.com>
2022-11-11 23:55:45 +0700

    edit

--- lib/components/menu.dart
+++ lib/components/menu.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'credit.dart';
 import 'history.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:dev_dtd/components/menu.dart';
+import './components/menu.dart';
 
 void main() {
   runApp(const MyApp());
